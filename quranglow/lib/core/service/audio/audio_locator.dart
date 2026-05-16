912b24ed51e7e766e818bc604c5b8cd3960ced91
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-05-16 14:58:55 +0300

    feat: implement audio handler locator and initialization service

--- quranglow/lib/core/service/audio/audio_locator.dart
+++ quranglow/lib/core/service/audio/audio_locator.dart
@@ -1,4 +1,4 @@
-// lib/core/audio/audio_locator.dart
+// lib/core/service/audio/audio_locator.dart
 import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
 import 'my_audio_handler.dart';
@@ -23,7 +23,7 @@ Future<void> initAudioHandler() async {
       androidNotificationChannelName: 'تشغيل الصوت',
       androidNotificationOngoing: false,
       androidStopForegroundOnPause: false,
-      androidNotificationIcon: 'mipmap/ic_launcher_round',
+      androidNotificationIcon: 'mipmap/ic_launcher',
       androidNotificationClickStartsActivity: true,
       notificationColor: Color(0xFF1B5E20),
       preloadArtwork: true,
